f934c9ad54518ba29f850ba245f7fe9535e3daa3
Elvis Vasconcelos <elvisrv@gmail.com>
2024-10-17 11:22:32 -0300

    chapter3

---
+++ src/lib.rs
@@ -0,0 +1,8 @@
+//! src/lib.rs
+pub mod configuration;
+pub mod routes;
+pub mod startup;
+
+// use actix_web::dev::Server;
+// use actix_web::{web, App, HttpResponse, HttpServer};
+// use std::net::TcpListener;
